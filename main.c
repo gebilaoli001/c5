6c0c013e4346ac7b1eb54503e7e0c102dac02470
litiansuo <2296923560@qq.com>
2019-01-18 10:17:53 +0800

    建立基本框架

--- main.c
+++ main.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
+struct student
+{
+    int yuwen;
+    int shuxue;
+    int yingyu;
+    char name[100];
+};
+int main()
+{
 
-int main(){
+    printf("xueshengchengjiguanlixitong\n");
 
-    printf("hello world!");
+    while (1)
+    {
+        printf("1---tianjiaxueshengchengji\n");
+        printf("2---shanchuxueshengchengji\n");
+        printf("3---xianshisuoyouxueshengchengji\n");
+        printf("4---yewu1:suoyouzongfenbujigedexuesheng(xiaoyu180)\n");
+        printf("5---yewu2:meiyikedoubujigedexuesheng\n");
+        printf("6---yewu3:xianshizongfenzuigaodexuesheng\n");
+        printf("7---yewu4:xianshipingjunfenzuigaoqirmeiyoubujigedexuesheng\n");
+        printf("8---tuichu\n");
+        printf("qingshuruxiangyinggongnengbianhao\n");
+
+        int code;
+        scanf("%d", & code);
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+        }
+        if (code == 6)
+        {
+        }
+        if (code == 7)
+        {
+        }
+        if (code == 8)
+        {
+            printf("dianjihuichetuichu\n");
+            break;
+        }
+    }
 
     return 0;
-
 }
